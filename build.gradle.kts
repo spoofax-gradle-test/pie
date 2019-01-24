ef2d0035845962db205ffcc390ea3769345fdb63
Gabriel Konat <gabrielkonat@gmail.com>
2019-01-24 16:34:25 +0100

    Update to Kotlin 1.3.20.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("org.metaborg.gradle.config.root-project") version "0.5.0"
   id("org.metaborg.gitonium") version "0.3.0"
-  kotlin("jvm") version "1.3.11" apply false // Apply only in sub-projects. Use version 1.3.11 for compatibility with Gradle 5.1.
+  kotlin("jvm") version "1.3.20" apply false // Apply only in sub-projects.
 }
 
 subprojects {
